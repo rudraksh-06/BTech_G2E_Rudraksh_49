fd1073e69d7085923c8b46b5d861ef33ca888f85
rudraksh-06 <25121cn114@glbitm.ac.in>
2026-02-24 15:04:07 +0530

    Create program17.c

---
+++ program17.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, sum = 0, remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while(num != 0)
+    {
+        remainder = num % 10;   
+        sum = sum + remainder;  
+        num = num / 10; 
+    }
+
+    printf("Sum of digits = %d", sum);
+
+    return 0;
+}
